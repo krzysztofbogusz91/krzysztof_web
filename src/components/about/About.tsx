d6176046849cd885b26f5575edf97360dd6f0c5e
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2019-10-15 12:26:42 +0200

    working on hex view

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -99,18 +99,17 @@ export default class About extends Component {
                 </li>
                 </ul>
                 </div>
-            <div className="section">
+            <div className="section about-section">
 
 
-                {/* hex gid */}
-                    <h2>About me</h2>
-                    <article>
+                    <h2 className="about-header">About me</h2>
+                    <article className="row row-1">
                         <span>Hi, I`m Krzysztof Poznań (Poland) based developer, but i mostly work remotely</span>
                     </article>
-                    <article>
+                    <article className="row row-2">
                         <span>My stack is based on JavaScript - I work with Angular2+, React, Vue and Vanilla.js</span>
                     </article>
-                    <article>
+                    <article className="row row-3">
                         <span>Node is my environment for backend dev, I`v done some projects with Express and MongoDb</span>
                     </article>
                 </div>
